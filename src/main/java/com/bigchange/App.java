341414b13b29bc5ac1c117088c6138472adf5e26
shanghai-Jerry <icbigdata@gmail.com>
2021-05-21 16:01:25 +0800

    update: create java webScoket client

--- src/main/java/com/bigchange/App.java
+++ src/main/java/com/bigchange/App.java
@@ -12,10 +12,9 @@ public class App {
    public static void main(String[] args ) {
 
         byte[] EOS = new byte[] {0x45, 0x4f, 0x53};
-        String token = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9" +
-           ".eyJleHAiOjE2MjE1ODg3NTUsImlzcyI6ImJiay1qd3QifQ.LKnZerDFDHjI0D0LwrRqWKDxEzAffSD4-BDRB4LdE1CvRES10tExpzmRGh_9-Kpa_D6mAf0-ATJLAWCAsBCrW0PxDVrMxtpce193fPJlQvXbwza6g35PdIF3JThe7Nie9KoMcF5uIcRAUyvqxutu1U-VweUKZPfPz4wT5L6hN9QBgSBwN1G_sqAZDqqm--u7Pkum99GdEmVby_gKv8wIYkWMNJ_McglLzkj7TgtTesbqfCuia0g-cSp09c87Cxu6CUzlI3AwE3fmqX0eZOBgmQkUcW_xfcHVk0POkVtvEWCeKyfTWlWQNMlmP6E4bWg-OeQbk0Fec3a6pK4aFUQ6ZQ";
+        String token = "Bearer xxx";
 
-       JavaClient javaClient = new JavaClient("wss://algapi.llsapp.com/api/ielts_speaking/stream");
+       JavaClient javaClient = new JavaClient("wss://xx_speaking/stream");
         WebSocketClient webSocketClient = javaClient.getClient(token);
         if (webSocketClient != null) {
             System.out.println("getWebSocketClient succeed");
@@ -31,7 +30,7 @@ public class App {
             System.out.printf("send meta:%s\n", jsonString);
             // send meta
             webSocketClient.send(jsonString.getBytes());
-            byte[] bytes = ReadFromFile.readFileByBytes("/Users/chaojiangyou/Desktop/question2.wav");
+            byte[] bytes = ReadFromFile.readFileByBytes("question2.wav");
             System.out.printf("send audio bytes:%d", bytes.length);
             // send audio
             webSocketClient.send(bytes);
